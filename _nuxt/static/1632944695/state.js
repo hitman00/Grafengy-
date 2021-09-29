2ff6169b93155c9670d75a7f5ecd8b050d5ad6a9
hitman00 <hitman00@users.noreply.github.com>
2021-09-29 19:44:56 +0000

    deploy: dfb9908fbf0a27591d0118d93700eb25b93af230

---
+++ _nuxt/static/1632944695/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgrafengy\u002F_nuxt\u002Fstatic\u002F1632944695",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fgrafengy\u002F",assetsPath:"\u002Fgrafengy\u002F_nuxt\u002F",cdnURL:a}}}}(null));
